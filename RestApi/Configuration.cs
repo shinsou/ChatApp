12346b94b43edb63816b9ecf7da04e2645a618f9
shinsou <henry.kaikko@gmail.com>
2022-01-27 23:53:44 +0200

    Add simplicity to program startup configurations
    - separate execute phase and configuration implementations

---
+++ RestApi/Configuration.cs
@@ -0,0 +1,30 @@
+﻿static class Configuration
+{
+    public static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder app)
+    {
+        var services = app.Services;
+
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        return app;
+    }
+
+    public static WebApplication ConfigureApi(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        return app;
+    }
+}

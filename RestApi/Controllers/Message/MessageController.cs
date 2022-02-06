c97d74e2643266bc8fbafd7bab65cea8754d8be2
shinsou <henry.kaikko@gmail.com>
2022-02-07 01:03:01 +0200

    Add mock-endpoint for retrieving messages

---
+++ RestApi/Controllers/Message/MessageController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestApi.Controllers.Message
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}

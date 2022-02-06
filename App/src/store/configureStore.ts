eef60fac3fd95127e4d30290cb419ada1ef7484f
shinsou <henry.kaikko@gmail.com>
2022-02-07 01:31:21 +0200

    Add stub api & theme configurations into configureStore

--- App/src/store/configureStore.ts
+++ App/src/store/configureStore.ts
@@ -5,16 +5,20 @@ import lobbyReducer from './lobbyReducer';
 import memberReducer from './memberReducer';
 import messageReducer from './messageReducer';
 import memberGroupsReducer from './memberGroupsReducer'
+//import { messageApi } from '../services/messageApi';
+//import themeReducer from './themeReducer';
 
 export const store = configureStore({
   reducer: {
+    //[messageApi.reducerPath]: messageApi.reducer,
+    //themeState: themeReducer,
     messages: messageReducer,
     lobbies: lobbyReducer,
     members: memberReducer,
     memberGroups: memberGroupsReducer
   },
     // serializableCheck: false -> will hide DateTime serialization validation check; most likely due to mockup data initialization
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false })
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false })/*.concat(messageApi.middleware)*/,
 });
 
 export type AppDispatch = typeof store.dispatch;

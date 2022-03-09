a929aeda93649717119185960cc10846adff4adb
shinsou <henry.kaikko@gmail.com>
2022-03-09 23:28:45 +0200

    Add basic properties and define message as auditable entity
    - apply parameterless constructor as default
    - apply constructor with id-parameter to support InMemory storing

--- Domain/Message.cs
+++ Domain/Message.cs
@@ -1,7 +1,13 @@
-﻿namespace Domain
-{
-    public class Message
-    {
+﻿using Domain.Abstract;
 
+namespace Domain
+{
+    public class Message : AuditableEntity<string>
+    {
+        public Message() { }
+        public Message(string id) : base(id) { }
+
+        public string Content { get; set; }
+        public string LobbyId { get; set; }
     }
 }

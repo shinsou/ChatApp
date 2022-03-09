0757fc1216043a69f333bd968c827d18e4fa4480
shinsou <henry.kaikko@gmail.com>
2022-03-09 23:14:23 +0200

    Add auditable entity base

---
+++ Domain/Abstract/AuditableEntity.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain.Abstract
+{
+    public abstract class AuditableEntity<T> : IEntity<T>, IAuditableEntity<string>, IUserAuditEntity<string>
+    {
+        public AuditableEntity() { }
+
+        public AuditableEntity(T id)
+        {
+            Id = id;
+        }
+
+        public T Id { get; }
+
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+
+        public string CreatedBy { get; set; }
+        public string UpdatedBy { get; set; }
+    }
+}

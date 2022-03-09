f4425608f34727af546d223cbfdded35e95facdd
shinsou <henry.kaikko@gmail.com>
2022-03-09 23:35:35 +0200

    Add auditable member entity in domain
    - apply parameterless constructor as default
    - apply constructor with id-parameter to support InMemory storing

---
+++ Domain/Member.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Abstract;
+
+namespace Domain
+{
+    public class Member : AuditableEntity<string>
+    {
+        public Member() { }
+        public Member(string id) : base(id) { }
+
+        public string Name { get; set; }
+    }
+}

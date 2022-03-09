fcd48f0425e605d53133fc0f6391404c81bc949e
shinsou <henry.kaikko@gmail.com>
2022-03-09 23:13:41 +0200

    Add abstract auditable entity

---
+++ Domain/Abstract/IAuditableEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Abstract
+{
+    public interface IAuditableEntity<T>
+    {
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}

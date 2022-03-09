0508883233aec08b921c82c6c86b3a93327742fd
shinsou <henry.kaikko@gmail.com>
2022-03-09 23:16:57 +0200

    Add abstract interactor class
    Base definition of clean architecture use case class that implements Model View Presenter pattern

---
+++ Domain/Abstract/IInteractor.cs
@@ -0,0 +1,22 @@
+﻿namespace Domain.Abstract
+{
+    public interface IInteractor<out TResult>
+    {
+        Task<TViewModel> HandleAsync<TViewModel>(IPresenter<TViewModel, TResult> presenter, CancellationToken cancellationToken = default);
+    }
+
+    public interface IInteractor<out TResult, in T>
+    {
+        Task<TViewModel> HandleAsync<TViewModel>(IPresenter<TViewModel, TResult> presenter, T input, CancellationToken cancellationToken = default);
+    }
+
+    public interface IInteractorAsync<out TResult>
+    {
+        Task<TViewModel> HandleAsync<TViewModel>(IPresenter<Task<TViewModel>, TResult> presenter, CancellationToken cancellationToken = default);
+    }
+
+    public interface IInteractorAsync<out TResult, in T>
+    {
+        Task<TViewModel> HandleAsync<TViewModel>(IPresenter<Task<TViewModel>, TResult> presenter, T input, CancellationToken cancellationToken = default);
+    }
+}

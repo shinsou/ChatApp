ac6a19156aacdd29240eb6631f091908db33ef18
shinsou <henry.kaikko@gmail.com>
2022-03-31 23:23:56 +0300

    Add introduction of LobbiesRepository interface to application

---
+++ src/Application/Abstracts/ILobbiesRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain;
+
+namespace Application.Abstracts
+{
+    public interface ILobbiesRepository
+    {
+        Task<IEnumerable<Lobby>> GetAsync(CancellationToken cancellationToken);
+    }
+}

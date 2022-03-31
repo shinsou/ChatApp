bda03e18af692949854d61b6f30129220bea050a
shinsou <henry.kaikko@gmail.com>
2022-03-31 23:24:34 +0300

    Add useCase for showing lobbies in application

---
+++ src/Application/UseCases/ShowAvailableLobbies/ShowAvailableLobbiesInteractor.cs
@@ -0,0 +1,23 @@
+﻿using Application.Abstracts;
+using Domain;
+using Domain.Abstract;
+
+namespace Application.UseCases.ShowAvailableLobbies
+{
+    public class ShowAvailableLobbiesInteractor : IInteractor<IEnumerable<Lobby>>
+    {
+        private readonly ILobbiesRepository lobbiesRepository;
+
+        public ShowAvailableLobbiesInteractor(ILobbiesRepository lobbiesRepository)
+        {
+            this.lobbiesRepository = lobbiesRepository;
+        }
+
+        public async Task<TViewModel> HandleAsync<TViewModel>(IPresenter<TViewModel, IEnumerable<Lobby>> presenter, CancellationToken cancellationToken = default)
+        {
+            var result = await lobbiesRepository.GetAsync(cancellationToken);
+
+            return presenter.Process(result);
+        }
+    }
+}
